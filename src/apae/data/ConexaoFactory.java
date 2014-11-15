b798fd9cb299255a53bc5c6f058f97581ef27804
Joao15Henrique <joao.henrique.coletto@gmail.com>
2014-11-15 13:37:27 -0200

    Fazenda a conexão com o Banco de Dados

--- src/apae/data/ConexaoFactory.java
+++ src/apae/data/ConexaoFactory.java
@@ -8,9 +8,15 @@ public class ConexaoFactory {
 	private static String conString = "";
 	private static Connection conexao = null;
 	
-	public static Connection getConexao() throws SQLException {
+	public static Connection getConexao() throws Exception {
 		if(conexao == null){
-			conexao = DriverManager.getConnection(conString);
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/ProjetoAPAE", "root", "");
+			} catch (ClassNotFoundException e) {
+				System.err.println("Erro nao conexão: " + e.getLocalizedMessage());
+				throw e;
+			}
 		}
 		return conexao;
 	}
